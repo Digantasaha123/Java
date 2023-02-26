ab8703e4c00bd261a9d00c7505c48d3efc43470f
Diganta123 <diganta15-4898@diu.edu.bd>
2023-02-26 21:57:57 +0600

    first

---
+++ lab_3/src/task1.java
@@ -0,0 +1,7 @@
+`import java.utill.Arrays;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("How many Numbers you want to insert? ");
+
+    }
+}
