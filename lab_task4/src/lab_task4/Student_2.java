11724253f26416ba55de6cd84e9d92ac9a1880f4
Diganta123 <diganta15-4898@diu.edu.bd>
2023-04-08 23:51:25 +0600

    lab 4

--- lab_task4/src/lab_task4/Student_2.java
+++ lab_task4/src/lab_task4/Student_2.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab_task4;
 
-/**
- *
- * @author Pc
- */
 public class Student_2 {
     
 }
